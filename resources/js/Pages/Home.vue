fbba9b3cf0470fd6cae5651c8cfae9e7deec53ee
dimasetiawans <office.dimasss@gmail.com>
2025-08-13 14:21:25 +0700

    bootstrap Inertia di frontend

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({ appName: String })
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Welcome to {{ appName }}</h1>
+    <inertia-link href="/users" class="underline">Go to Users</inertia-link>
+  </div>
+</template>
