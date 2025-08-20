10465065bd4e8cd3246dcf599ba6315ca4a9ceb2
dimasetiawans <office.dimasss@gmail.com>
2025-08-20 09:41:44 +0700

    install SB-Admin Theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 // resources/js/app.js
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import '../assets/vendor/jquery/jquery.min.js'
+import '../assets/vendor/bootstrap/js/bootstrap.bundle.min.js'
+import '../assets/vendor/jquery-easing/jquery.easing.min.js'
+import '../assets/js/sb-admin-2.min.js'
 
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
